6ee672eb5c7cae852f9ec5b8b6606ac4ec88d057
OlarteLAB <olarte.siul@gmail.com>
2021-10-19 10:22:27 -0500

    centre el formulario, se ve un poco mejor. No se hace mas arreglos, pues esto es una de las primeras pruebas que realice al empezar el curso

---
+++ js/script.js
@@ -0,0 +1,24 @@
+function validardatos() {
+    /* Comentario */
+
+    /*alert("Hola mundo")
+    prompt("Ingrese su nombre")
+    console.log("Hola mundo")
+    document.write("Hola mundo") */
+
+    let nombre = document.getElementById('name').value
+
+    alert(nombre)
+
+    let email = document.getElementById('email').value
+
+    alert(email)
+
+    let edad = document.getElementById('edad').value
+
+    alert(edad)
+
+    let telefono = document.getElementById('telefono').value
+
+    alert(telefono)
+}
